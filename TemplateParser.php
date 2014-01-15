f5562f84ec4a67088d04a4f148123f4da59634fa
harry <harry@harry-VirtualBox.(none)>
2014-01-15 16:35:05 +0800

    seperate with 2 class

--- TemplateParser.php
+++ TemplateParser.php
@@ -3,14 +3,20 @@
 
 require_once('../simplehtmldom/simple_html_dom.php');
 
-$html = 'aaaaa<div data-widget-type="form" data-widget-id="52d5fb7ab2979" data-variable="">dasfasdfasdfasdf</div>bbbbb';
-$tp = new TemplateParser($html);
-echo '<textarea cols="150" rows="50">' . $tp->parseTest() . '</textarea>';
+$html = '<div data-widget-type="form" data-widget-id="52d5fb7ab2979" data-variable=\'{a:10,b:"strimg",c:true}\'>dasfasdfasdfasdf</div>
+<p>Hello</p>
+<div data-widget-type="form" data-widget-id="52d5fb7ab29e5" data-variable="">dasfasdfasdfasdf</div>
+<div data-widget-type="plugin" data-widget-id="blabla" data-variable="">dasfasdfasdfasdf</div>';
+$tp = new CkeditorTemplateParser($html);
+echo 'Input: <br /><textarea cols="150" rows="25">' . $tp->getOrigin() . '</textarea><br />';
+echo 'Output: <br /><textarea cols="150" rows="25">' . $tp->parseTest() . '</textarea><br />';
 
-class TemplateParser
+
+
+abstract class TemplateParser
 {
-    private $origin = '';
-    private $plugin_file_guid_mappers = array(
+    protected $origin = '';
+    protected $plugin_file_guid_mappers = array(
         '52d5fb7ab2827' => 'addtosafesenderlist.php',
         '52d5fb7ab289e' => 'agenda_service.php',
         '52d5fb7ab290b' => 'edit_data.php',
@@ -26,6 +32,7 @@ class TemplateParser
         '52d5fb7ab2d4e' => 'subscribe.php',
         '52d5fb7ab2db9' => 'unsubscribe.php'
     );
+    protected $divider = "========================";
 
     function __construct($origin) {
         $this->origin = $origin;
@@ -35,23 +42,35 @@ class TemplateParser
       return $this->origin;
     }
 
+    abstract public function parseTest();
+}
+
+
+
+class CkeditorTemplateParser extends TemplateParser
+{
     public function parseTest() {
         $dom = str_get_html($this->origin);
+        $output = '';
         foreach($dom->find('div[data-widget-id]') as $e) {
-            $attrs = $e->getAllAttributes();
-            $attrs_php = '$attrs = array(' . "\n";
-            foreach ($attrs as $key => $value) {
-                $attrs_php .= '  $' . $key . ' => \'' . $value . '\',' . "\n";
+            $output .= 'widget-id:\'' . $e->getAttribute('data-widget-id') . '\'' . "\n";
+            $output .= 'widget-type:\'' . $e->getAttribute('data-widget-type') . '\'' . "\n";
+            $output .= 'widget-name:\'' . (isset($this->plugin_file_guid_mappers[$e->getAttribute('data-widget-id')]) ? $this->plugin_file_guid_mappers[$e->getAttribute('data-widget-id')] : 'undefined') . '\'' . "\n";
+            $value = $e->getAttribute('data-variable');
+            if (substr($value, 0, 1) == '{') {
+                $real_value = "\n" . '{' . "\n";
+                $inner_value = trim($value, '{}');
+                $inner_values = explode(',', $inner_value);
+                foreach ($inner_values as $v) {
+                    $real_value .= '  ' . $v . "\n";
+                }
+                $real_value .= '}';
+            } else {
+                $real_value = '\'' . $value . '\'';
             }
-            $attrs_php .= ');';
-            $e->outertext =
-                "\n" . 
-                '<?php' . "\n" . 
-                $attrs_php . "\n" .
-                'include("' . $this->plugin_file_guid_mappers[$e->getAttribute('data-widget-id')] . '");' . "\n" .
-                '?>' . "\n";
+            $output .= 'variable:' . $real_value . '' . "\n";
+            $output .= $this->divider . "\n";
         }
-        return $dom->save();
+        return $output;
     }
-
 }
